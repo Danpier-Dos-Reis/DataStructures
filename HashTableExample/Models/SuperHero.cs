1d85886083aa5293471f29d9bec7ab2ca7894049
Danpier Dos Reis <Danpier.Morales@hispatec.com>
2023-09-11 14:10:16 -0300

    Example using class HashTable

---
+++ HashTableExample/Models/SuperHero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableExample.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; } public string? Name { get; set; } public string Powers { get; set; }
+
+        public SuperHero(int id, string name, string powers)
+        {
+            Id = id;
+            Name = name;
+            Powers = powers;
+        }
+    }
+}
