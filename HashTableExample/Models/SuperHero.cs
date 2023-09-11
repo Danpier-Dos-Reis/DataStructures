3f174a6728314fc429239f2317de93e4a33bfca0
Danpier Dos Reis <Danpier.Morales@hispatec.com>
2023-09-11 14:24:49 -0300

    Example using Dictionary

--- HashTableExample/Models/SuperHero.cs
+++ HashTableExample/Models/SuperHero.cs
@@ -6,15 +6,42 @@ using System.Threading.Tasks;
 
 namespace HashTableExample.Models
 {
+    //public class SuperHero
+    //{
+    //    public int Id { get; set; } public string? Name { get; set; } public string Powers { get; set; }
+
+    //    public SuperHero(int id, string name, string powers)
+    //    {
+    //        Id = id;
+    //        Name = name;
+    //        Powers = powers;
+    //    }
+    //}
+
     public class SuperHero
     {
-        public int Id { get; set; } public string? Name { get; set; } public string Powers { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string Powers { get; set; }
 
         public SuperHero(int id, string name, string powers)
         {
             Id = id;
             Name = name;
             Powers = powers;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is SuperHero otherHero)
+            {
+                return Id == otherHero.Id && Name == otherHero.Name && Powers == otherHero.Powers;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^ Name.GetHashCode() ^ Powers.GetHashCode();
         }
     }
 }
--- HashTableExample/Program.cs
+++ HashTableExample/Program.cs
@@ -3,26 +3,31 @@
 using HashTableExample.Models;
 using System.Collections;
 
-Hashtable heroestable = new Hashtable();
+Dictionary<int, SuperHero> heroestable = new Dictionary<int, SuperHero>();
 
 SuperHero[] heroes = {
     new SuperHero(1,"Super Man","Fly, Strenght, Laser"),
     new SuperHero(2,"Wonder Woman", "Fly, Strenght"),
     new SuperHero(3,"Flash","Speed Force"),
-    new SuperHero(4,"Grenn Lantern","DoSomething"),
+    new SuperHero(4,"Green Lantern","DoSomething"),
     new SuperHero(5,"Goku","Fly, Kamekameha, Teletransportation"),
     new SuperHero(6,"Naruto","Rasengan, Kagebushin no Jutsu")
 };
 
-//Adding SuperHeroes to hashtable
-foreach (var hero in heroes) { heroestable.Add(hero.Id , hero); }
+//Adding SuperHeroes to Dictionary
+foreach (var hero in heroes) { heroestable.Add(hero.Id, hero); }
 
-//Get an superHero from hashtable
-SuperHero newGoku = (SuperHero)heroestable[5];
+//Get an superHero from Dictionary
+SuperHero newGoku = (SuperHero)heroestable[6];
 
-//hashtable contain the number 10 like a key?
-string contain10 = heroestable.Contains(10) == true ? "yes":"no";
+//Dictionary contain the number 10 like a key?
+string contain10 = heroestable.ContainsKey(10) == true ? "yes" : "no";
+
+//Are Flash in the Dictionary?
+SuperHero flash = new SuperHero(3, "Flash", "Speed Force");
+string containValue = heroestable.ContainsValue(flash) == true ? "yes" : "no";
 
 Console.WriteLine($"Table Selection => Id: {newGoku.Id}, Name: {newGoku.Name}" +
-                                            $"\n\tPowers => {newGoku.Powers}" +Environment.NewLine);
-Console.WriteLine($"table contain the 10 key? \n\t  R => {contain10}");
+                                            $"\n\tPowers => {newGoku.Powers}" + Environment.NewLine);
+Console.WriteLine($"table contain the 10 key? \n\t  R => {contain10}" + Environment.NewLine);
+Console.WriteLine($"Are Flash in the table?" + $"\n\tR => {containValue}");
